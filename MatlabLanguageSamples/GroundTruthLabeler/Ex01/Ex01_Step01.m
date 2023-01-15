343365195a9298be0c31357298fed0ea04be16bd
Christopher Lum <christopher.w.lum@outlook.com>
2023-01-15 07:27:52 -0800

    Ex02

---
+++ MatlabLanguageSamples/GroundTruthLabeler/Ex01/Ex01_Step01.m
@@ -0,0 +1,15 @@
+%Explore using the Ground Truth Labeler app
+%
+%Example data located at C:\Program Files\MATLAB\R2021b\toolbox\driving\drivingdata
+%
+%Christopher Lum
+%lum@uw.edu
+
+%Version History
+%01/14/23: Created
+
+clear
+clc
+close all
+
+disp('DONE!')
---
+++ MatlabLanguageSamples/GroundTruthLabeler/Ex02/Step01.m
@@ -0,0 +1,15 @@
+%Follow example at https://www.mathworks.com/help/driving/ug/train-a-deep-learning-vehicle-detector.html
+%
+%Data located at C:\Program Files\MATLAB\R2021b\examples\deeplearning_shared\data
+%
+%Christopher Lum
+%lum@uw.edu
+
+%Version History
+%01/14/23: Created
+
+clear
+clc
+close all
+
+disp('DONE!')
