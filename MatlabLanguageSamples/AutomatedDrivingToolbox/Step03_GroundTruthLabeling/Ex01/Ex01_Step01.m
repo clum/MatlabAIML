e33040473253500482938827e374cc5896b631ca
Christopher Lum <christopher.w.lum@outlook.com>
2023-01-15 17:23:15 -0800

    updates

---
+++ MatlabLanguageSamples/AutomatedDrivingToolbox/Step03_GroundTruthLabeling/Ex01/Ex01_Step01.m
@@ -0,0 +1,17 @@
+%Explore using the Ground Truth Labeler app
+%
+%Follows example at https://www.mathworks.com/help/driving/ug/load-ground-truth-signals-to-label.html
+%
+%Example data located at C:\Program Files\MATLAB\R2021b\toolbox\driving\drivingdata
+%
+%Christopher Lum
+%lum@uw.edu
+
+%Version History
+%01/14/23: Created
+
+clear
+clc
+close all
+
+disp('DONE!')
