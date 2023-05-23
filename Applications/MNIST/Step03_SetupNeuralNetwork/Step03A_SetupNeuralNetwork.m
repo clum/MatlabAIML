18cca3aad0ad072907e09445e00ca385eea7cbb1
Christopher Lum <lum@uw.edu>
2023-05-22 21:05:32 -0700

    Update through step03.

---
+++ Applications/MNIST/Step03_SetupNeuralNetwork/Step03A_SetupNeuralNetwork.m
@@ -0,0 +1,64 @@
+%Setup the NeuralNetwork object
+%
+%Christopher Lum
+%lum@uw.edu
+
+%Version History
+%05/22/23: Created
+
+clear
+clc
+close all
+
+tic
+
+%% User selections
+%1 = 
+scenarioSelection = 1;
+
+switch scenarioSelection
+    case 1
+        nodesPerLayer = [28*28 50 10];
+        nn = NeuralNetwork(nodesPerLayer);
+        nn.SetActivationFunctionAtAllLayers(ActivationFunctionID.Sigmoid);
+        
+        %Initialize weights between random weights/biases uniformly between [-1,1]
+        %Set weights (note the first element of weights is the weights
+        %incoming to layer 2)
+        weights = {};
+        for L=1:length(nodesPerLayer)-1
+            %Randomize between [-1,1]
+            weights{L} = 2*(rand(nodesPerLayer(L+1),nodesPerLayer(L)) - 0.5);
+        end
+        
+        for L=2:length(nodesPerLayer)
+            nn.SetWeightsIncomingToLayer(L,weights{L-1});
+        end
+        
+        %Set biases (note the first element of biases is the biases in
+        %layer 2)
+        biases = {};
+        for L=1:length(nodesPerLayer)-1
+            %Randomize between [-1,1]
+            biases{L} = 2*(rand(nodesPerLayer(L+1),1) - 0.5);
+        end
+        
+        for L=2:length(nodesPerLayer)
+            nn.SetBiasesAtLayer(L,biases{L-1});
+        end
+        
+    otherwise
+        error('Unsupported scenarioSelection')
+end
+
+%% Save network
+outputFile = ['NeuralNetworkScenario',num2str(scenarioSelection),'.mat'];
+saveVars = {
+    'nn'
+    };
+s = SaveVarsString(outputFile,saveVars);
+eval(s);
+disp(['Saved to ',outputFile])
+
+toc
+disp('DONE!')
